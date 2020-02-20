028c9ecdf0cf8ff5c9ab4e1bb165f1643b8b58eb
Enid-Sky <enid.sky@protonmail.com>
2020-02-19 16:31:15 -0800

    added loops and logic

--- todoListJavascript.js
+++ todoListJavascript.js
@@ -148,4 +148,161 @@ var todoList = {
   }
 };
 
-123
+
+//*** Add a for loop and log each todoText in list form***
+
+
+var todoList = {
+  todos: [],
+  displayTodos: function () {
+    console.log('My todos:'); //removed logging entire array list
+    //create a for loop, to iterate over each item in todos
+    for (var i = 0; i < this.todos.length; i++) {
+      //log the todoText at each index, one at a time
+      console.log(this.todos[i].todoText); //access todo text prop.
+    }
+  },
+  addTodo: function (todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function (position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function (position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function (position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  }
+};
+
+
+
+//*** Add edge case, condition if todos is empty***
+
+var todoList = {
+  todos: [],
+  displayTodos: function () {
+    //test if todos is empty
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty');
+    } else {
+      console.log('My todos:');
+      for (var i = 0; i < this.todos.length; i++) {
+        console.log(this.todos[i].todoText);
+      }
+    }
+  },
+  addTodo: function (todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function (position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function (position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function (position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  }
+};
+
+//*** displayTodos should show .completed***
+
+var todoList = {
+  todos: [],
+  displayTodos: function () {
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty');
+    } else {
+      console.log('My todos:');
+      for (var i = 0; i < this.todos.length; i++) {
+        //check with if statement if .completed is true
+        if (this.todos[i].completed === true) {
+          //print with x
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          //print with ( )
+          console.log('( )', this.todos[i].todoText);
+        }
+      }
+    }
+  },
+  addTodo: function (todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function (position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function (position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function (position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  }
+};
+
+//
+
+
+var todoList = {
+  todos: [],
+  displayTodos: function () {
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty');
+    } else {
+      console.log('My todos:');
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('( )', this.todos[i].todoText);
+        }
+      }
+    }
+  },
+  addTodo: function (todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function (position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function (position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function (position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  }
+};
